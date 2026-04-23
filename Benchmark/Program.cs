c0105f18a97b519ef773e5298658f1c489bdfef6
Evgeny Yafarkin <yafarkin@gmail.com>
2026-04-23 19:20:44 +0400

    Добавлен ручной вариант;

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,10 +1,33 @@
+using System.Diagnostics;
 using Benchmark;
 using BenchmarkDotNet.Running;
 
 //BenchmarkRunner.Run<JitEngineBenchmark>();
-BenchmarkRunner.Run<FullpipeBenchmark>();
+//BenchmarkRunner.Run<FullpipeBenchmark>();
 
-// var b = new FullpipeBenchmark();
-// b.Setup();
-// await b.RunScript_FullRandom();
-// b.Cleanup();
+const int WarmupCycles = 1000;
+const int TestCycles = 10_000;
+
+var b = new FullpipeBenchmark();
+b.Setup();
+
+Console.WriteLine("Warming up...");
+for (var i = 0; i < WarmupCycles; i++)
+{
+    await b.RunScript_FullRandom();
+}
+
+Console.WriteLine("Measuring...");
+var sw = Stopwatch.StartNew();
+for (var i = 0; i < TestCycles; i++)
+{
+    await b.RunScript_FullRandom();
+}
+sw.Stop();
+
+b.Cleanup();
+
+var avgMs = (double)sw.ElapsedMilliseconds / TestCycles;
+Console.WriteLine($"Total: {sw.ElapsedMilliseconds} ms");
+Console.WriteLine($"Average: {avgMs:F3} ms/request");
+Console.WriteLine($"Requests/sec: {1000 / avgMs:F0}");
